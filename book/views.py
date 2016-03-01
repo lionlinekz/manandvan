cee7b07aabddc7a8acea7a7053b2a7c016269012
kalitanov altynbek <alt@MacBook-Air-kalitanov.local>
2016-03-02 03:57:56 +0600

    impl maps.google.com api for calculating distance and basic formula for price calc

--- book/views.py
+++ book/views.py
@@ -1,8 +1,58 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests, yaml
+
+
+
+
+key = 'AIzaSyAxcsY7PiyouoQ2okEbNEoqWgHrLR7yxts' # (maps.google.com) api simple key change to your own
+
+def book(request):
+
+	context_dict = {}
+
+	if request.method == 'POST':
+		print 'hello'
+		print request.POST
+		body = request.POST
+		orig = body.__getitem__('from')
+		dest = body.__getitem__('to')
+		date = body.__getitem__('date')
+
+		u='https://maps.googleapis.com/maps/api/distancematrix/json?origins='+orig+'&destinations='+dest+'&mode=driving&language=en-GB&key='+key
+
+		map_data=yaml.safe_load(requests.get(u).text)
+
+		if date == '112358':
+			if str(map_data['status'])=='OK':
+				context_dict['duration'] = map_data['rows'][0]['elements'][0]['duration']['text']
+				context_dict['price'] = int(float(map_data['rows'][0]['elements'][0]['distance']['value'])*0.001)
+				context_dict['distance'] = map_data['rows'][0]['elements'][0]['distance']['text']
+				context_dict['origin_addresses'] = map_data['origin_addresses'][0]
+				context_dict['destination_addresses'] = map_data['destination_addresses'][0]
+				return render(request, 'search-results.html', context_dict)
+
+		
+	else:
+		print 'empty'
+	return render(request, 'index.html')
+
+
+'''{u'status': u'OK', u'rows': [{u'elements': [{u'duration': {u'text': u'3 hours 45 mins', u'value': 13509}, u'distance': {u'text': u'347 km', u'value': 346652}, u'status': u'OK'}]}], u'origin_addresses': [u'Boston, MA, USA'], u'destination_addresses': [u'New York, NY, USA']}'''
+
+
+
+
+
+
+
 
 def index(request):
 	return render(request, 'index.html')
 # Create your views here.
+
+
 
 def aboutus(request):
 	return render(request, 'about-us.html')
--- manandvan/urls.py
+++ manandvan/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib import admin
 from book import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^book/', include('book.urls')),
+
+
+    #tmp templates
+    
     url(r'^$', views.index),
+    url(r'^index.html', views.index),
 
     url(r'^about-us.html', views.aboutus),
     url(r'^becomeadriver.html', views.becomeadriver),
@@ -31,5 +37,6 @@ urlpatterns = [
     url(r'^privacy.html', views.privacy),
     url(r'^service-area.html', views.servicearea),
     url(r'^terms.html', views.terms),
+
 
 ]
