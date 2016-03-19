f7fb64afa6fb977b760d593881f82885d4aa1c63
kalitanov altynbek <alt@MacBook-Air-kalitanov.local>
2016-03-19 19:19:48 +0600

    price model

--- pageapp/admin.py
+++ pageapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from pageapp.models import HourlyRateForVanType
 
 # Register your models here.
+admin.site.register(HourlyRateForVanType)
--- pageapp/models.py
+++ pageapp/models.py
@@ -2,4 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class HourlyRateForVanType(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    weekday_customer_loading_price = models.IntegerField(default=0)
+    weekday_customer_loading_min_price = models.IntegerField(default=0)
+    weekend_customer_loading_price = models.IntegerField(default=0)
+    weekend_customer_loading_min_price = models.IntegerField(default=0)
+
+    weekday_one_man_loading_price = models.IntegerField(default=0)
+    weekday_one_man_loading_min_price = models.IntegerField(default=0)
+    weekend_one_man_loading_price = models.IntegerField(default=0)
+    weekend_one_man_loading_min_price = models.IntegerField(default=0)
+
+    weekday_two_man_loading_price = models.IntegerField(default=0)
+    weekday_two_man_loading_min_price = models.IntegerField(default=0)
+    weekend_two_man_loading_price = models.IntegerField(default=0)
+    weekend_two_man_loading_min_price = models.IntegerField(default=0)
+    
+    weekday_three_man_loading_price = models.IntegerField(default=0)
+    weekday_three_man_loading_min_price = models.IntegerField(default=0)
+    weekend_three_man_loading_price = models.IntegerField(default=0)
+    weekend_three_man_loading_min_price = models.IntegerField(default=0)
+
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
